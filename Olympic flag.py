86b66812d53f43dc895b3ac276bbbb6cdda6af2b
victenna <39585970+victenna@users.noreply.github.com>
2019-11-06 13:42:23 -0500

    Add files via upload

---
+++ Olympic flag.py
@@ -0,0 +1,35 @@
+import turtle
+t = turtle.Turtle()
+t.pensize(20)
+
+t.color('green')
+t.circle(100)
+
+t.color('gold')
+t.penup()
+t.setposition(-240,0)
+t.pendown()
+t.circle(100)
+
+t.color('red')
+t.penup()
+t.setposition(120,120)
+t.pendown()
+t.circle(100)
+
+t.color('black')
+t.penup()
+t.setposition(-120,120)
+t.pendown()
+t.circle(100)
+
+t.color('blue')
+t.penup()
+t.setposition(-360,120)
+t.pendown()
+t.circle(100)
+
+
+
+
+
