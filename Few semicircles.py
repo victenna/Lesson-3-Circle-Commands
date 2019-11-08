07090083b6ce2548255a2bc4846e8546d252d268
victenna <39585970+victenna@users.noreply.github.com>
2019-11-08 16:22:22 -0500

    Add files via upload

---
+++ Few semicircles.py
@@ -0,0 +1,39 @@
+import turtle
+t = turtle.Turtle()
+
+t.up()
+t.goto(-450, 0)
+t.down()
+
+t.setheading(270)
+t.color('red')
+t.begin_fill()
+t.circle(50, 180)
+t.end_fill()
+t.color('navy')
+t.begin_fill()
+t.circle(-50, 180)
+t.end_fill()
+
+t.setheading(270)
+t.color('red')
+t.begin_fill()
+t.circle(50, 180)
+t.end_fill()
+t.color('navy')
+t.begin_fill()
+t.circle(-50, 180)
+t.end_fill()
+
+t.setheading(270)
+t.color('red')
+t.begin_fill()
+t.circle(50, 180)
+t.end_fill()
+t.color('navy')
+t.begin_fill()
+t.circle(-50, 180)
+t.end_fill()
+
+t.screen.exitonclick()
+ 
---
+++ Orange slice.py
@@ -0,0 +1,21 @@
+import turtle
+t=turtle.Turtle()
+t.pensize(10)
+t.left(90)
+t.color('yellow', 'red')
+t.begin_fill()
+t.circle(100,180)
+t.end_fill()
+
+
+
+t.pensize(10)
+t.color('green', 'violet')
+t.setheading(0)
+t.setheading(-90)
+t.begin_fill()
+t.circle(98,180)
+t.end_fill()
+
+
+
